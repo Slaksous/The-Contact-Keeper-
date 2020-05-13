a7aa265cc0becc68d500353e1ca1f06f5c17509b
Lloyd <lloydjvrensburg@gmail.com>
2020-05-13 21:36:10 +0200

    Backend Setup Completed

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
+// Conntect Databse
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => {
-    res.json({msg: 'Welcome to the contact keeper APi'})
+    res.send('Hello from server');
 });
 
 // Define Routes
